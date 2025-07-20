faa0d779fc1004c2ecacefe53421684857f12e5f
SERDUN <dmitriiserdun@gmail.com>
2025-07-20 17:56:00 +0300

    feat: add injectable decorator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { container } from "./core";
 import { UsersController } from "./features/users/users.controller";
+import { UsersService } from "./features/users/users.service";
 
+// Directly registering UsersController
 container.register(UsersController, UsersController);
 let userController = container.resolve(UsersController);
 console.log(userController);
+
+// Registered by decorator
+let userService = container.resolve(UsersService);
+console.log(userService)
