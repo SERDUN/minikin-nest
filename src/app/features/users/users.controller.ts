24ad730fcf51f851d7f23805762ccb7a6956034b
SERDUN <dmitriiserdun@gmail.com>
2025-07-21 15:32:39 +0300

    chore: add controller mocks

--- src/app/features/users/users.controller.ts
+++ src/app/features/users/users.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from "../../../core";
+import { Controller, Delete, Get, Post } from "../../../core";
+import { Param } from "../../../core/decorators/param";
 
 @Controller('/users')
 export class UsersController {
     @Get('/')
     list() {
         return "users list";
+    }
+
+    @Get('/:id')
+    getUser(id: string) {
+        return `user ${id}`;
+    }
+
+    @Post('/')
+    createUser(user: any) {
+        return `user created with data: ${JSON.stringify(user)}`;
+    }
+
+    @Delete('/:id')
+    deleteUser(@Param('id') id: string) {
+        return `user ${id} deleted`;
     }
 }
